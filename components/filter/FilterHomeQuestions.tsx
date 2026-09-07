b96a2fb8c5a7fcc87990db22c6b2081453a8effb
Mostafa Beder <mostafabder2@gmail.com>
2026-09-07 19:01:02 +0300

    Adjust spacing in FilterHomeQuestions component

--- components/filter/FilterHomeQuestions.tsx
+++ components/filter/FilterHomeQuestions.tsx
@@ -3,7 +3,7 @@ import FilterOption from "./FilterOption";
 
 function FilterHomeQuestions() {
   return (
-    <ul className="flex flex-wrap items-center gap-2 sm:gap-4">
+    <ul className="flex flex-wrap items-center gap-1 sm:gap-4">
       {FILTER_OPTIONS.map((filterOption) => (
         <FilterOption key={filterOption.value} filterOption={filterOption} />
       ))}
